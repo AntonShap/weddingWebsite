91d45cd24be1b734b789790edbe38fe5c447cf9e
Georgi Despotov <79579263+GeorgiDespotov@users.noreply.github.com>
2021-05-31 19:27:49 +0300

    resize step one

--- CodeGeorge/src/app.js
+++ CodeGeorge/src/app.js
@@ -57,9 +57,9 @@ function carousel() {
 
     //arrange the slides next to one another
 
-    //slides[0].style.left=slideWidth * 0 + 'px';
-    //slides[1].style.left=slideWidth * 1 + 'px';
-    //slides[2].style.left=slideWidth * 2 + 'px';
+    slides[0].style.left=slideWidth * 0 + 'px';
+    slides[1].style.left=slideWidth * 1 + 'px';
+    slides[2].style.left=slideWidth * 2 + 'px';
 
     const setSlidePosition = (slide, index) => {
         slide.style.left = slideWidth * index + 'px';
